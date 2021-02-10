20750ade97130d77dd5c6416e03279867f1a9400
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-09 20:08:11 -0500

    finished keys

--- server/src/shared/transposeChart.ts
+++ server/src/shared/transposeChart.ts
@@ -1,154 +1,17 @@
 import Chart from 'src/models/Chart';
-
-type KeyTypes =
-  | 'C'
-  | 'F'
-  | 'Bb'
-  | 'Eb'
-  | 'Ab'
-  | 'Db'
-  | 'Gb'
-  | 'B'
-  | 'E'
-  | 'A'
-  | 'D'
-  | 'G';
-
-const keys = {
-  C: {
-    '%': '%',
-    '1': 'C',
-    b2: 'Db',
-    '2': 'D',
-    b3: 'Eb',
-    '3': 'E',
-    '4': 'F',
-    '#4': 'F#',
-    '5': 'G',
-    b6: 'Ab',
-    '6': 'A',
-    b7: 'Bb',
-    '7': 'B',
-  },
-  F: {
-    '%': '%',
-    '1': 'F',
-    '2': 'G',
-    '3': 'A',
-    '4': 'Bb',
-    '5': 'C',
-    '6': 'D',
-    '7': 'E',
-  },
-  Bb: {
-    '%': '%',
-    '1': 'Bb',
-    '2': 'C',
-    '3': 'D',
-    '4': 'Eb',
-    '5': 'F',
-    '6': 'G',
-    '7': 'A',
-  },
-  Eb: {
-    '%': '%',
-    '1': 'Eb',
-    '2': 'F',
-    '3': 'G',
-    '4': 'Ab',
-    '5': 'Bb',
-    '6': 'C',
-    '7': 'D',
-  },
-  Ab: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-  Db: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-  Gb: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-  B: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-  E: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-  A: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-  D: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-  G: {
-    '%': '%',
-    '1': 'Ab',
-    '2': 'Bb',
-    '3': 'C',
-    '4': 'Db',
-    '5': 'Eb',
-    '6': 'F',
-    '7': 'G',
-  },
-};
+import keys, { KeyTypes } from './keys';
 
 const transposeChart = (chart: Chart, key: KeyTypes): Chart => {
   const transposedBars = chart.bars.chords.map((chord) => {
     const newChord = { ...chord };
     const functionalNumber = newChord.functionalNumber;
+    const chordQuality = newChord.chordQuality;
+    const isSeventhChord = newChord.isSeventhChord;
     const keyName: { [key: string]: string } = keys[key];
-    const displayName = keyName[functionalNumber];
-    newChord.displayName = displayName;
+    const chordName = keyName[functionalNumber];
+    newChord.displayName = `${chordName} ${chordQuality}${
+      isSeventhChord ? '7' : ''
+    }`;
     return newChord;
   });
 
