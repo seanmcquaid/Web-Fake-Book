6ba28e05ccb2e22871034bb817eaef92bb7b6d0e
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-12 16:02:26 -0500

    finished reducer set value tests

--- client/src/context/__tests__/AddChartContext/reducer.spec.ts
+++ client/src/context/__tests__/AddChartContext/reducer.spec.ts
@@ -1,5 +1,66 @@
+import reducer from '../../AddChartContext/reducer';
+import {
+  Actions,
+  ActionTypes,
+  ChartInfoTypes,
+} from '../../AddChartContext/types';
+
 describe('Add Chart Context - Reducer', () => {
-  describe('SET_VALUE', () => {});
+  describe('SET_VALUE', () => {
+    it('numberOfBars changes and adds the appropriate number of bars with the appropriate amount of chords', () => {
+      const state: ChartInfoTypes = {
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      };
+      const action: ActionTypes = {
+        type: Actions.SET_VALUE,
+        payload: {
+          key: 'numberOfBars',
+          value: 12,
+        },
+      };
+
+      const result = reducer(state, action);
+
+      expect(result.bars.length).toEqual(12);
+      expect(result.bars[0].chords.length).toEqual(4);
+    });
+
+    it('Other key value to change', () => {
+      const state: ChartInfoTypes = {
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      };
+      const action: ActionTypes = {
+        type: Actions.SET_VALUE,
+        payload: {
+          key: 'beatsPerMeasure',
+          value: 6,
+        },
+      };
+
+      const result = reducer(state, action);
+      expect(result).toEqual({
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [],
+        beatsPerMeasure: 6,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      });
+    });
+  });
 
   it('UPDATE_CHORD_IN_BAR', () => {});
 
