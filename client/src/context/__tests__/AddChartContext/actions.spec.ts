1850e08f96ce28838e3901e2046189dead043f79
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-12 15:50:31 -0500

    finished actions tests

--- client/src/context/__tests__/AddChartContext/actions.spec.ts
+++ client/src/context/__tests__/AddChartContext/actions.spec.ts
@@ -1,7 +1,35 @@
+import {
+  addChartSuccess,
+  setValue,
+  updateChordInBar,
+} from '../../AddChartContext/actions';
+
 describe('Add Chart Context - Actions', () => {
-  it('SET_VALUE', () => {});
+  it('SET_VALUE', () => {
+    expect(setValue('numberOfBars', 12)).toEqual({
+      type: 'SET_VALUE',
+      payload: {
+        key: 'numberOfBars',
+        value: 12,
+      },
+    });
+  });
 
-  it('UPDATE_CHORD_IN_BAR', () => {});
+  it('UPDATE_CHORD_IN_BAR', () => {
+    expect(updateChordInBar(0, 0, 'functionalNumber', 'b2')).toEqual({
+      payload: {
+        barIndex: 0,
+        beatIndex: 0,
+        key: 'functionalNumber',
+        value: 'b2',
+      },
+      type: 'UPDATE_CHORD_IN_BAR',
+    });
+  });
 
-  it('ADD_CHART_SUCCESS', () => {});
+  it('ADD_CHART_SUCCESS', () => {
+    expect(addChartSuccess()).toEqual({
+      type: 'ADD_CHART_SUCCESS',
+    });
+  });
 });
