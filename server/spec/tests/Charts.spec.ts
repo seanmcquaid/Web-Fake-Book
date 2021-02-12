3e128e71014ecbdb087bcff3fbb70362968090c5
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-11 22:16:29 -0500

    set up first successful integration test, going to finish these tonight

--- server/spec/tests/Charts.spec.ts
+++ server/spec/tests/Charts.spec.ts
@@ -6,6 +6,7 @@ import ChartDao from '@daos/Chart/ChartDao';
 import { pErr } from '@shared/functions';
 import { paramMissingError } from '@shared/constants';
 import { IReqBody, IResponse } from '../support/types';
+import { Document, Query } from 'mongoose';
 
 describe('Charts Routes', () => {
   const { BAD_REQUEST, CREATED, OK } = StatusCodes;
@@ -18,79 +19,116 @@ describe('Charts Routes', () => {
 
   describe('POST - /charts/add', () => {
     it('chartExists already returns bad status code', (done) => {
+      const body = {
+        name: 'Tune Name Here',
+        defaultKey: 'F',
+        numberOfBars: 1,
+        bars: [
+          {
+            chords: [
+              {
+                functionalNumber: 'b2',
+                chordQuality: 'Major',
+                isSeventhChord: true,
+              },
+              {
+                functionalNumber: '1',
+                chordQuality: 'Minor',
+                isSeventhChord: true,
+              },
+              {
+                functionalNumber: '%',
+                chordQuality: '%',
+                isSeventhChord: false,
+              },
+              {
+                functionalNumber: '%',
+                chordQuality: '%',
+                isSeventhChord: false,
+              },
+            ],
+          },
+        ],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      };
+
+      spyOn(ChartDao, 'findOne').and.returnValue(Promise.resolve(true) as any);
+
       spyOn(ChartDao, 'create').and.returnValue();
       agent
         .post('/charts/add')
         .type('form')
-        .send({})
+        .send(body)
         .end((err: Error, res: IResponse) => {
           pErr(err);
-          expect(res.status).toBe(CREATED);
-          expect(res.body.error).toBeUndefined();
+          expect(res.status).toBe(BAD_REQUEST);
+          expect(res.body.error).toEqual('This chart already exists!');
           done();
         });
     });
 
-    it('Chart is created successfully', (done) => {
-      spyOn(ChartDao, 'create').and.returnValue();
-      agent
-        .post('/charts/add')
-        .type('form')
-        .send({})
-        .end((err: Error, res: IResponse) => {
-          pErr(err);
-          expect(res.status).toBe(CREATED);
-          expect(res.body.error).toBeUndefined();
-          done();
-        });
-    });
+    // it('Chart is created successfully', (done) => {
+    //   spyOn(ChartDao, 'create').and.returnValue();
+    //   agent
+    //     .post('/charts/add')
+    //     .type('form')
+    //     .send({})
+    //     .end((err: Error, res: IResponse) => {
+    //       pErr(err);
+    //       expect(res.status).toBe(CREATED);
+    //       expect(res.body.error).toBeUndefined();
+    //       done();
+    //     });
+    // });
   });
 
-  describe('GET - /charts/all', () => {
-    it('All charts return', (done) => {
-      agent.get('/charts/all').end((err: Error, res: IResponse) => {
-        pErr(err);
-        expect(res.status).toBe(OK);
-        done();
-      });
-    });
-  });
+  // describe('GET - /charts/all', () => {
+  //   it('All charts return', (done) => {
+  //     agent.get('/charts/all').end((err: Error, res: IResponse) => {
+  //       pErr(err);
+  //       expect(res.status).toBe(OK);
+  //       done();
+  //     });
+  //   });
+  // });
 
-  describe('GET - /chart/:id/:key*?', () => {
-    it('', (done) => {
-      agent.get('/charts/all').end((err: Error, res: IResponse) => {
-        pErr(err);
-        expect(res.status).toBe(OK);
-        done();
-      });
-    });
+  // describe('GET - /chart/:id/:key*?', () => {
+  //   it('', (done) => {
+  //     agent.get('/charts/all').end((err: Error, res: IResponse) => {
+  //       pErr(err);
+  //       expect(res.status).toBe(OK);
+  //       done();
+  //     });
+  //   });
 
-    it('', (done) => {
-      agent.get('/charts/all').end((err: Error, res: IResponse) => {
-        pErr(err);
-        expect(res.status).toBe(OK);
-        done();
-      });
-    });
-  });
+  //   it('', (done) => {
+  //     agent.get('/charts/all').end((err: Error, res: IResponse) => {
+  //       pErr(err);
+  //       expect(res.status).toBe(OK);
+  //       done();
+  //     });
+  //   });
+  // });
 
-  describe('PUT - /edit/:id', () => {
-    it('', (done) => {
-      agent.get('/charts/all').end((err: Error, res: IResponse) => {
-        pErr(err);
-        expect(res.status).toBe(OK);
-        done();
-      });
-    });
-  });
+  // describe('PUT - /edit/:id', () => {
+  //   it('', (done) => {
+  //     agent.get('/charts/all').end((err: Error, res: IResponse) => {
+  //       pErr(err);
+  //       expect(res.status).toBe(OK);
+  //       done();
+  //     });
+  //   });
+  // });
 
-  describe('DELETE - /delete/:id', () => {
-    it('', (done) => {
-      agent.get('/charts/all').end((err: Error, res: IResponse) => {
-        pErr(err);
-        expect(res.status).toBe(OK);
-        done();
-      });
-    });
-  });
+  // describe('DELETE - /delete/:id', () => {
+  //   it('', (done) => {
+  //     agent.get('/charts/all').end((err: Error, res: IResponse) => {
+  //       pErr(err);
+  //       expect(res.status).toBe(OK);
+  //       done();
+  //     });
+  //   });
+  // });
 });
