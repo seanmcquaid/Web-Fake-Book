7bdb779451db87eb99329d0c4a6694dc350ea7df
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-12 20:50:21 -0500

    fixed UI bug, might consider refactoring the reducer later

--- client/src/context/AddChartContext/reducer.ts
+++ client/src/context/AddChartContext/reducer.ts
@@ -37,6 +37,31 @@ const reducer = (
           bars,
         };
       }
+
+      if (action.payload.key === 'beatsPerMeasure') {
+        let bars: BarType[] = [];
+
+        for (let i = 0; i < state.numberOfBars; i++) {
+          const bar: BarType = { chords: [] };
+          for (let j = 0; j < action.payload.value; j++) {
+            const chord: ChordTypes = {
+              functionalNumber: '%',
+              chordQuality: '%',
+              isSeventhChord: false,
+            };
+            bar.chords = [...bar.chords, chord];
+          }
+          bars = [...bars, bar];
+        }
+
+        const beatsPerMeasure = action.payload.value as number;
+
+        return {
+          ...state,
+          beatsPerMeasure,
+          bars,
+        };
+      }
       return {
         ...state,
         [action.payload.key]: action.payload.value,
