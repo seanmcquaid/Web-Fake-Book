a82f9cdbf8dfb9bf0b5b9254bc86d1dfaca35f66
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-22 13:25:40 -0500

    finished edit chart input reducer tests

--- client/src/containers/EditChart/EditChartInput/reducer/___tests___/actions.spec.ts
+++ client/src/containers/EditChart/EditChartInput/reducer/___tests___/actions.spec.ts
@@ -1,7 +1,43 @@
+import {
+  loadChartSuccessAction,
+  loadingChartAction,
+  updateChordInBarAction,
+} from '../actions';
+
 describe('EditChartInput - Actions', () => {
-  it('loadingChartAction', () => {});
+  it('loadingChartAction', () => {
+    expect(loadingChartAction()).toEqual({
+      type: 'LOADING',
+    });
+  });
 
-  it('updateChordInBarAction', () => {});
+  it('updateChordInBarAction', () => {
+    expect(updateChordInBarAction(0, 0, 'isSeventhChord', true)).toEqual({
+      type: 'UPDATE_CHORD_IN_BAR',
+      payload: {
+        barIndex: 0,
+        beatIndex: 0,
+        key: 'isSeventhChord',
+        value: true,
+      },
+    });
+  });
 
-  it('loadChartSuccessAction', () => {});
+  it('loadChartSuccessAction', () => {
+    const chartInfo = {
+      name: 'NAME',
+      bars: [],
+      numberOfBars: 0,
+      noteValuePerBeat: 4,
+      beatsPerMeasure: 4,
+      defaultKey: 'C',
+      genre: 'Standard',
+    };
+    expect(loadChartSuccessAction(chartInfo)).toEqual({
+      type: 'LOAD_CHART_SUCCESS',
+      payload: {
+        chartInfo,
+      },
+    });
+  });
 });
--- client/src/containers/EditChart/EditChartInput/reducer/___tests___/reducer.spec.ts
+++ client/src/containers/EditChart/EditChartInput/reducer/___tests___/reducer.spec.ts
@@ -1,7 +1,141 @@
+import reducer from '..';
+import {
+  EditChartInputActions,
+  EditChartInputActionTypes,
+  EditChartInputStateTypes,
+} from '../types';
+
 describe('EditChartInput - Reducer', () => {
-  it('LOADING', () => {});
+  it('LOADING', () => {
+    const state: EditChartInputStateTypes = {
+      isLoading: false,
+      chartInfo: {
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      },
+    };
 
-  it('UPDATE_CHORD_IN_BAR', () => {});
+    const action: EditChartInputActionTypes = {
+      type: EditChartInputActions.LOADING,
+    };
 
-  it('LOAD_CHART_SUCCESS', () => {});
+    expect(reducer(state, action)).toEqual({
+      isLoading: true,
+      chartInfo: {
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      },
+    });
+  });
+
+  it('UPDATE_CHORD_IN_BAR', () => {
+    const state: EditChartInputStateTypes = {
+      isLoading: false,
+      chartInfo: {
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [
+          {
+            chords: [
+              {
+                functionalNumber: '1',
+                chordQuality: 'Major',
+                isSeventhChord: false,
+              },
+            ],
+          },
+        ],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      },
+    };
+
+    const action: EditChartInputActionTypes = {
+      type: EditChartInputActions.UPDATE_CHORD_IN_BAR,
+      payload: {
+        barIndex: 0,
+        beatIndex: 0,
+        key: 'isSeventhChord',
+        value: true,
+      },
+    };
+
+    expect(reducer(state, action)).toEqual({
+      isLoading: false,
+      chartInfo: {
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [
+          {
+            chords: [
+              {
+                functionalNumber: '1',
+                chordQuality: 'Major',
+                isSeventhChord: true,
+              },
+            ],
+          },
+        ],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      },
+    });
+  });
+
+  it('LOAD_CHART_SUCCESS', () => {
+    const state: EditChartInputStateTypes = {
+      isLoading: true,
+      chartInfo: {
+        name: '',
+        defaultKey: 'C',
+        numberOfBars: 0,
+        bars: [],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      },
+    };
+
+    const action: EditChartInputActionTypes = {
+      type: EditChartInputActions.LOAD_CHART_SUCCESS,
+      payload: {
+        chartInfo: {
+          name: 'New Chart',
+          defaultKey: 'Db',
+          numberOfBars: 12,
+          bars: [],
+          beatsPerMeasure: 4,
+          noteValuePerBeat: 4,
+          genre: 'Standard',
+        },
+      },
+    };
+
+    expect(reducer(state, action)).toEqual({
+      isLoading: false,
+      chartInfo: {
+        name: 'New Chart',
+        defaultKey: 'Db',
+        numberOfBars: 12,
+        bars: [],
+        beatsPerMeasure: 4,
+        noteValuePerBeat: 4,
+        genre: 'Standard',
+      },
+    });
+  });
 });
