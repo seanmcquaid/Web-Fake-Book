68a29d1f866039653735d68bb7783f0227572b1a
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-22 17:10:00 -0500

    fixed charts cypress tests

--- client/cypress/integration/Charts.spec.ts
+++ client/cypress/integration/Charts.spec.ts
@@ -10,16 +10,16 @@ describe('Charts', () => {
   it('Filter charts by search terms lessens amount of results', () => {
     cy.get('.sc-jSgupP').type('Flat');
 
-    cy.get('.sc-bYEvPH').should('have.text', 'Page 1 of 1');
+    cy.get('.sc-kLgntA').should('have.text', 'Page 1 of 1');
   });
 
   it('Next page button disabled when user gets to last page', () => {
-    cy.get('.sc-ezrdKe > :nth-child(3)').click();
-    cy.get('.sc-ezrdKe > :nth-child(3)').should('be.disabled');
+    cy.get('.sc-bYEvPH > :nth-child(3)').click();
+    cy.get('.sc-bYEvPH > :nth-child(3)').should('be.disabled');
   });
 
   it('Prev page button disabled on load', () => {
-    cy.get('.sc-ezrdKe > :nth-child(1)').should('be.disabled');
+    cy.get('.sc-bYEvPH > :nth-child(1)').should('be.disabled');
   });
 });
 
