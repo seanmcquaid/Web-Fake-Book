675e9037bdbdf51a7df3818e97bded6be0b6d283
seanmcquaid <sean.mcquaid2@verizon.net>
2021-02-26 19:53:25 -0500

    decided to eliminate repeat symbol as it creates a lot of issues with design

--- server/src/shared/transposeChart.ts
+++ server/src/shared/transposeChart.ts
@@ -3,20 +3,22 @@ import keys, { KeyTypes } from './keys';
 
 const transposeChart = (chart: Chart, key: KeyTypes): Chart => {
   const transposedBars = chart.bars.map((bar) => {
+    const chords = bar.chords.map((chord) => {
+      const newChord = { ...chord };
+      const functionalNumber = newChord.functionalNumber;
+      const chordQuality = newChord.chordQuality;
+      const isSeventhChord = newChord.isSeventhChord;
+      const keyName: { [key: string]: string } = keys[key];
+      const chordName = keyName[functionalNumber];
+      newChord.displayName =
+        chordName === '%'
+          ? ''
+          : `${chordName} ${chordQuality}${isSeventhChord ? ' 7' : ''}`;
+      return newChord;
+    });
+
     return {
-      chords: bar.chords.map((chord) => {
-        const newChord = { ...chord };
-        const functionalNumber = newChord.functionalNumber;
-        const chordQuality = newChord.chordQuality;
-        const isSeventhChord = newChord.isSeventhChord;
-        const keyName: { [key: string]: string } = keys[key];
-        const chordName = keyName[functionalNumber];
-        newChord.displayName =
-          chordName === '%'
-            ? '%'
-            : `${chordName} ${chordQuality}${isSeventhChord ? ' 7' : ''}`;
-        return newChord;
-      }),
+      chords,
     };
   });
 
